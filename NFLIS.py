f708f712516c30d6af7b07e5fba60ff406160bbe
Lela Bones <lbones1@gulls.salisbury.edu>
2019-01-26 10:49:18 -0500

    Printing to file

--- NFLIS.py
+++ NFLIS.py
@@ -15,26 +15,45 @@ states = pd.Series(df["State"]).unique()
 
 # This function prints the county, year and amount of reports
 def report2():
+    file = open("Report2", "w")
     for substance in substances:
         #saving data to graph
-        print(substance)
+        file.write(substance + "\n")
 
         for year in years:
-            print(year)
+            file.write(str(year) + "\n")
             for county in counties:
                 #picking the data we need from specific states, counties, drugs, etc.
                 dr = np.array(df.loc[(df["FIPS_Combined"] == county) & (df["SubstanceName"] == substance) & (df["YYYY"] == year), "DrugReports"])
-                print(county, ", Drug Reports = ", dr)
-            print()
+                file.write(str(county) + ", Drug Reports = " + str(dr) + "\n")
+            file.write("\n")
+    file.close()
 
 #This function allows us to look at data for step one
 def report1():
+    file = open("Report1", "w")
     for year in years:
+        file.write(str(year) + "\n")
         for county in counties:
             info = np.array(df.loc[(df["FIPS_Combined"] == county) & (df["YYYY"] == year), "TotalDrugReportsCounty"])
-        print(county, ", Drug Reports = ", info)
+            if info.size == 0:
+                file.write(str(county) + ", Drug Reports = 0 \n")
+            else:
+                file.write(str(county) + ", Drug Reports = " +  str(info[0]) + "\n")
+    file.close()
+
+
+#Plotting Arguments
+plot_args = [{'c': 'red', 'linestyle': '-'},
+             {'c': 'green', 'linestyle': '-'},
+             {'c': 'blue', 'linestyle': '-'},
+             {'c': 'red', 'linestyle': '--'},
+             {'c': 'green', 'linestyle': '--'},
+             {'c': 'blue', 'linestyle': '--'},
+             {'c': 'black', 'linestyle': '-'},
+             {'c': 'black', 'linestyle': '--'}]
 
 
 
 report1()
-report2()
+#report2()
