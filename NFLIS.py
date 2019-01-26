f47c8e5cab9c4eb4f5a52520cf0482179c85f5dc
Lela Bones <lbones1@gulls.salisbury.edu>
2019-01-25 21:20:52 -0500

    Report1 and Report 2 complete

--- NFLIS.py
+++ NFLIS.py
@@ -1,25 +1,40 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 #  reading in data from NFLIS file
 df = pd.read_excel('Data/MCM_NFLIS_Data.xlsx', sheet_name='Data')
 #  print(data)
 
 #grabbing the substance unique to each county
-counties =  pd.Series(df["COUNTY"]).unique()
+counties =  pd.Series(df["FIPS_Combined"]).unique()
 substances = pd.Series(df["SubstanceName"]).unique()
 years = pd.Series(df["YYYY"]).unique()
 states = pd.Series(df["State"]).unique()
 
-for substance in substances:
-    #saving data to graph
-    data = []
-    print(substance)
-    for year in years:
-        print(year)
-        for state in states:
+
+# This function prints the county, year and amount of reports
+def report2():
+    for substance in substances:
+        #saving data to graph
+        print(substance)
+
+        for year in years:
+            print(year)
             for county in counties:
                 #picking the data we need from specific states, counties, drugs, etc.
-                dr = np.array(df.loc[(df["State"] == state) & (df["COUNTY"] == county) & (df["SubstanceName"] == substance) & (df["YYYY"] == year), "DrugReports"])
+                dr = np.array(df.loc[(df["FIPS_Combined"] == county) & (df["SubstanceName"] == substance) & (df["YYYY"] == year), "DrugReports"])
                 print(county, ", Drug Reports = ", dr)
-                #appending data to array
-                data.append(dr)
+            print()
+
+#This function allows us to look at data for step one
+def report1():
+    for year in years:
+        for county in counties:
+            info = np.array(df.loc[(df["FIPS_Combined"] == county) & (df["YYYY"] == year), "TotalDrugReportsCounty"])
+        print(county, ", Drug Reports = ", info)
+
+
+
+report1()
+report2()
